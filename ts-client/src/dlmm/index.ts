05889c2e292480c2fff6af240960213e09b55c70
codewithgun <codewithgun@gmail.com>
2025-04-14 22:11:37 +0800

    fix: wrong fee payer fn initializeMultiplePositionAndAddLiquidityByStrategy

--- ts-client/src/dlmm/index.ts
+++ ts-client/src/dlmm/index.ts
@@ -3134,14 +3134,14 @@ export class DLMM {
     return {
       initPositionIxs: initPositionIxs.map((ixs) => {
         return new Transaction({
-          feePayer: this.lbPair.creator,
+          feePayer: user,
           blockhash,
           lastValidBlockHeight,
         }).add(...ixs);
       }),
       addLiquidityIxs: addLiquidityIxs.map((ixs) => {
         return new Transaction({
-          feePayer: this.lbPair.creator,
+          feePayer: user,
           blockhash,
           lastValidBlockHeight,
         }).add(...ixs);
